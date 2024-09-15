7e58b9d2dde4b05f8baf42f6907dc4fbf6ab088d
catarina p <catarinampestana@ua.pt>
2024-09-15 19:41:49 +0100

    Created a page thta is to be responsible for changing the background image every session

---
+++ js/services/background.js
@@ -0,0 +1,10 @@
+cont background = {
+    "../assets/forest.jpeg",
+    "../assets/glaciar.jpeg",
+    "../assets/gradient.jpeg",
+    "../assets/img.jpeg",
+    "../assets/river.jpeg",
+    "../assets/ball.jpeg"
+}
+
+//Implement logic to change background change with every session
